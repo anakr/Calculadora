3b1c165e8286f0b34af4e0495e317395f90f1e83
Ana <islas.rocha.ak@gmail.com>
2019-03-13 08:21:43 -0700

    Variables

--- app/src/main/java/anakaren/com/calculadora/MainActivity.java
+++ app/src/main/java/anakaren/com/calculadora/MainActivity.java
@@ -2,12 +2,36 @@ package anakaren.com.calculadora;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button2
+    Button button3
+    Button button4
+    Button button5
+    Button button6
+    Button button7
+    Button button8
+    Button button9
+    Button button10
+    Button button11
+    Button button12
+    Button button13
+    Button button14
+    Button button15
+    Button button16
+    Button button17
+    Button button18
+    Button button19
+    Button button20
+    Button button21
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        
     }
 }

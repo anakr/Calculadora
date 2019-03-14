8a0cfc56c43e66177f82410ab7f0dd19e47c433c
Ana <islas.rocha.ak@gmail.com>
2019-03-13 21:57:04 -0700

    Variables

--- app/src/main/java/anakaren/com/calculadora/MainActivity.java
+++ app/src/main/java/anakaren/com/calculadora/MainActivity.java
@@ -6,32 +6,63 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button button2
-    Button button3
-    Button button4
-    Button button5
-    Button button6
-    Button button7
-    Button button8
-    Button button9
-    Button button10
-    Button button11
-    Button button12
-    Button button13
-    Button button14
-    Button button15
-    Button button16
-    Button button17
-    Button button18
-    Button button19
-    Button button20
-    Button button21
+    Button seven;
+    Button nine;
+    Button eight;
+    Button divide;
+    Button four;
+    Button six;
+    Button five;
+    Button multiply;
+    Button one;
+    Button three;
+    Button two;
+    Button minus;
+    Button equal;
+    Button decimal;
+    Button zero;
+    Button plus;
+    Button cos;
+    Button allclear;
+    Button sin;
+    Button exponent;
+
+    double suma;
+    double resta;
+    double multiplicacion;
+    double division;
+    double potencia;
+    double seno;
+    double coseno;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
+        seven = findViewById(R.id.button2) ;
+        nine = findViewById(R.id.button3);
+        eight = findViewById(R.id.button4);
+        divide = findViewById(R.id.button5);
+        four = findViewById(R.id.button6);
+        six = findViewById(R.id.button7);
+        five = findViewById(R.id.button8);
+        multiply = findViewById(R.id.button9);
+        one = findViewById(R.id.button10);
+        three = findViewById(R.id.button11);
+        two = findViewById(R.id.button12);
+        minus = findViewById(R.id.button13);
+        equal = findViewById(R.id.button14);
+        decimal = findViewById(R.id.button15);
+        zero = findViewById(R.id.button16);
+        plus = findViewById(R.id.button17);
+        cos = findViewById(R.id.button18);
+        allclear = findViewById(R.id.button19);
+        sin = findViewById(R.id.button20);
+        exponent = findViewById(R.id.button21);
+
+
+
+
     }
 }
